2b83cc6e12b96750bb321e99d6008fd7e22aab42
I-Jurij <yjurij@gmail.com>
2025-01-27 21:37:40 +0300

    up

--- src/geolocation.js
+++ src/geolocation.js
@@ -1,28 +1,27 @@
-import { html } from "./js/html.js";
+import { htmlReplace, htmlInfo } from "./js/html.js";
 import { LS } from './js/localStorage.js'
-import { showLocality } from './js/showLocality.js'
 import { localityToServer } from './js/localityToServer.js'
 
-html();
-
 document.addEventListener('DOMContentLoaded', () => {
+    htmlReplace();
     // search in localstorage keeped data with user location
     let locality_from_ls = LS.getLocalStorage('locality');
 
     if (locality_from_ls) {
         if (city_from_back && !city_from_back.toLowerCase().includes(locality_from_ls.city.toLowerCase())) {
             localityToServer(url_location_to_server_js, csrf, locality_from_ls.city, locality_from_ls.region)
-            showLocality({ city: locality_from_ls.city, region: locality_from_ls.region });
+            htmlInfo({ city: locality_from_ls.city, region: locality_from_ls.region });
             console.log('localityToServer')
         }
         console.log('none action')
     } else {
         if (city_from_back == unknown_location) {
-            // get city from coord by browser.navigator.
+            // get city from coord throw browser.navigator
             //getLoc();
             console.log('getLoc by coord')
         } else {
             LS.setLocality({ city: city_from_back, region: region_from_back });
+            console.log('Locality to local storage')
         }
     }
 });
--- src/js/html.js
+++ src/js/html.js
@@ -11,7 +11,8 @@ function newData(elem_id, new_inner) {
 		elem.innerHTML = new_inner;
 	}
 }
-export function html() {
+
+export function htmlReplace() {
 	let inner_message_footer = '<label for="show_city_select" class="button" id="shoose_location">\
 						Выбрать\
 					</label>\
@@ -39,24 +40,47 @@ export function html() {
 								<label for="show_city_select" class="button dangerous">\
 									Закрыть\
 								</label>';
+	showHideModal("modal_1", 'hide');
+	newData('footer_city_message', inner_message_footer);
+	newData('section_city_choice', inner_choice_section);
+	newData('footer_city_choice', inner_choice_footer);
 
-	document.addEventListener('DOMContentLoaded', function () {
+	document.getElementById('shoose_location').onpointerdown = function () {
+		showHideModal("show_city_select", 'show');
 		showHideModal("modal_1", 'hide');
-		newData('footer_city_message', inner_message_footer);
-		newData('section_city_choice', inner_choice_section);
-		newData('footer_city_choice', inner_choice_footer);
+	};
 
-		document.getElementById('shoose_location').onpointerdown = function () {
-			showHideModal("show_city_select", 'show');
-			showHideModal("modal_1", 'hide');
-		};
-
-		/* <!-- js for esc on modal --> */
-		document.onkeydown = function (event) {
-			if (event.key == "Escape") {
-				var mods = document.querySelectorAll('.modal > [type=checkbox]');
-				[].forEach.call(mods, function (mod) { mod.checked = false; });
-			}
+	/* <!-- js for esc on modal --> */
+	document.onkeydown = function (event) {
+		if (event.key == "Escape") {
+			var mods = document.querySelectorAll('.modal > [type=checkbox]');
+			[].forEach.call(mods, function (mod) { mod.checked = false; });
 		}
-	});
+	}
 };
+
+function regionWithoutCity(city, region) {
+	let adr = '';
+	if (typeof region == 'string' && region && region.includes(city + ' ')) {
+		adr = '<div class="my2">' + region + '</div>';
+	} else if (typeof region == 'string' && region && !region.includes(city + ' ') && region != city) {
+		adr = '<div class="mt2">' + city + '</div><div class="mb2">' + region + '</div>';
+	} else {
+		adr = '<div class="my2">' + city + '.</div>'
+	}
+	return adr;
+}
+
+export function htmlInfo({ city, region }) {
+	let info = '';
+	if (city && typeof city == 'string') {
+		info = 'Ваше местоположение: ' + regionWithoutCity(city, region) + ' Если нет - выберите его, нажав на кнопку "Выбрать"';
+		newData('location', city);
+		newData('clients_location_message', info);
+		// showHideModal("modal_1", 'show');
+	} else {
+		info = 'Ваше местоположение неизвестно. </br>Выберите его, нажав на кнопку "Выбрать"';
+		newData('clients_location_message', info);
+		showHideModal("modal_1", 'hide');
+	}
+}
