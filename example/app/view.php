ac7b7207147695b5c584bed0e42bfbbd7e961ee6
IJurij <yjurij@gmail.com>
2025-02-28 22:33:56 +0300

    up

--- example/app/view.php
+++ example/app/view.php
@@ -25,8 +25,7 @@
 			let url_location_to_server_js = 'location_to_server_js'; 
 		</script>
 		<!--  <script type="module" src="geolocation.es.js"></script> -->
-		<!--  <script src="assets/geolocation.iife.min.js"></script> -->
-		<script src="https://cdn.statically.io/gh/i-jurij/geolocation/refs/heads/main/build/geolocation.iife.min.js"></script>
+		<script src="geolocation.iife.min.js"></script>
 
 		</body>
 		</html>
